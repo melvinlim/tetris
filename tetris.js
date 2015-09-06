b39e1abf34500e5bbb3a81327a49e4e5b1969580
Melvin Lim <lim.dongxing@gmail.com>
2015-09-05 18:37:01 -0700

    falling block

--- tetris.js
+++ tetris.js
@@ -1,31 +1,51 @@
 var i;
-var NROWS=30;
-var NCOLS=60;
+var NROWS=60;
+var NCOLS=30;
 var PXSZ=10;
 var arr=[];
-for(i=0;i<NCOLS;i++){
-	for(j=0;j<NROWS;j++){
-		arr[j+(i*NROWS)]=0;
+for(i=0;i<NROWS;i++){
+	for(j=0;j<NCOLS;j++){
+		arr[j+(i*NCOLS)]=0;
 	}
 }
-window.alert(arr);
+for(j=5;j<9;j++){
+	arr[j]=1;
+}
+//window.alert(arr);
 var canvas=document.getElementById("myCanvas");
 var cv=canvas.getContext("2d");
 
-cv.fillStyle="#ffffff";
-cv.fillRect(50,50,200,500);
-
-setInterval(updateGame,1000);
+setInterval(updateGame,100);
 
 function updateGame(){
+	updateBlocks();
 	drawBackground();
 	drawBlocks();
 }
 
+function updateBlocks(){
+	for(i=(NROWS-2);i>=0;i--){
+		for(j=0;j<NCOLS;j++){
+			if(arr[j+(i*NCOLS)]>0){
+				arr[j+((i+1)*NCOLS)]=1;
+				arr[j+(i*NCOLS)]=0;
+			}
+		}
+	}
+}
+
 function drawBlocks(){
+	cv.fillStyle="#ffffff";
+	for(i=0;i<NROWS;i++){
+		for(j=0;j<NCOLS;j++){
+			if(arr[j+(i*NCOLS)]>0){
+				cv.fillRect(j*PXSZ,i*PXSZ,PXSZ,PXSZ);
+			}
+		}
+	}
 }
 
 function drawBackground(){
 	cv.fillStyle="#000000";
-	cv.fillRect(0,0,NROWS*PXSZ,NCOLS*PXSZ);
+	cv.fillRect(0,0,NCOLS*PXSZ,NROWS*PXSZ);
 }
